ba4a70d3745ccd8fb5fd3261bff1ecc8cd54e7dd
Michał Czarnecki <offsystem@o2.pl>
2018-09-15 13:09:12 +0200

    end task4

--- js/script.js
+++ js/script.js
@@ -1,109 +1,109 @@
-'use strict';
 
-var mainCarousel = document.querySelector('.main-carousel');
-var slidesScript = document.getElementById('slideScript').innerHTML;
-var conButtons = document.querySelector('.container-buttons');
-var progressBar = document.querySelector('.progress-bar');
-var previousWrappedButton = document.querySelector('.restart');
-var dataSlidesLength = dataSlides.length;
-var allSlides = '';
+/************* MUSTACHE pêtla *****************/
+var listItems = '';
+var templateItem = document.getElementById('template-product-item').innerHTML;
 
-for (var i = 0; i < dataSlidesLength; i++) {
-    allSlides += Mustache.render(slidesScript, dataSlides[i]);
-}
-mainCarousel.insertAdjacentHTML('afterbegin', allSlides);
+for(var i = 0; i < carouselData.length; i++){
+		listItems += Mustache.render(templateItem, carouselData[i]);
+    
+	};
 
-var flkty = new Flickity(mainCarousel, {
-    cellAlign: 'left',
-    contain: true,
-    pageDots: false,
+results.insertAdjacentHTML('beforeend', listItems);
+
+/************ CAROUSEL **********/
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  hash: true
+
 });
 
-new Flickity(conButtons, {
-    asNavFor: ".main-carousel",
-    pageDots: false,
-    contain: true,
-    prevNextButtons: false
-});
+/***************** MAPA **********************/
 
-previousWrappedButton.addEventListener('click', function () {
-    flkty.select(0);
-});
+window.initMap = function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: carouselData[0].coords
+  });
+  
 
-flkty.on('scroll', function (progress) {
-    progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + '%';
-});
+  function createfunc(i) {
+    return function() { flkty.select(i); };
+};
 
-(function(){ 
-	// Zapiszemy sobei w zmiennej odwo³anie do elementu z id="infos", w którym bêdziemy wyœwietlaæ komunikaty po klikniêciu markera. 
-	
-	var infos = document.getElementById('infos');
-	
-	// Definiujemy funkcjê initMap tak samo jak wczeœniej. 
-	
-  	window.initMap = function() {
-		// Zdefiniujemy parê dodatkowych wspó³rzêdnych dla dodatkowych markerów. 
-		var warsaw = {lat: 52.229, lng: 21.011};
-		var berlin = {lat: 52.518, lng: 13.404};
-		var prague = {lat: 50.075, lng: 14.435};
-		var paris = {lat: 48.856, lng: 2.351};
-		var amsterdam = {lat: 52.369, lng: 4.895};
-		
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: warsaw
-		});
-		
-		var markerOne = new google.maps.Marker({
-			position: warsaw,
-			map: map
-		});
-		
-		// Po dodaniu markera mo¿emy u¿yæ jego metody addListener:
-		
-		markerOne.addListener('click', function(){
-			// Wewn¹trz funcji wpisujemy kod, który ma siê wykonaæ po klikniêciu markera. W tym przyk³adzie wyœwietlimy tekst na stronie. 
-			infos.innerHTML = 'You clicked Warsaw';
-		});		
-		
-		// Dodajemy jeszcze dwa markery, aby sprawdziæ czy na pewno klikniêcie ka¿dego z nich wyœwietli inny tekst. 
-		
-		var markerTwo = new google.maps.Marker({
-			position: berlin,
-			map: map
-		});
 
-		markerTwo.addListener('click', function(){
-			infos.innerHTML = 'You clicked Berlin';
-		});		
-		
-		var markerThree = new google.maps.Marker({
-			position: prague,
-			map: map
-		});
-		
-		markerThree.addListener('click', function(){
-			infos.innerHTML = 'You clicked Prague';
-		});	
-		
-		var markerFour = new google.maps.Marker({
-			position: paris,
-			map: map
-		});
-		
-		markerFour.addListener('click', function(){
-			infos.innerHTML = 'You clicked Paris';
-		});	
+ var marker =[];
+  for(var i = 0; i < carouselData.length; i++) {
+    marker[i] = new google.maps.Marker({
+      position: carouselData[i].coords,
+      map: map     
+    });
+    marker[i].addListener('click', createfunc(i));
+  }
 
-		var markerFive = new google.maps.Marker({
-			position: amsterdam,
-			map: map
-		});
-		
-		markerFive.addListener('click', function(){
-			infos.innerHTML = 'You clicked Amsterdam';
-		});	
-	}; 
-	
-})();  
+
+  flkty.on('change', function(index) {
+          smoothPanAndZoom(map, 8, carouselData[index].coords);
+  });
+
+};
+
+/************** FUNKCJA SMOOTH ****************/
+  var smoothPanAndZoom = function(map, zoom, coords){
+    
+    var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+    jumpZoom = Math.min(jumpZoom, zoom -1);
+    jumpZoom = Math.max(jumpZoom, 3);
+ 
+    smoothZoom(map, jumpZoom, function(){
+
+      smoothPan(map, coords, function(){
+        smoothZoom(map, zoom); 
+      });
+    });
+  };
+  
+  var smoothZoom = function(map, zoom, callback) {
+    var startingZoom = map.getZoom();
+    var steps = Math.abs(startingZoom - zoom);
+    
+    if(!steps) {
+      if(callback) {
+        callback();
+      }
+      return;
+    }
+
+    var stepChange = - (startingZoom - zoom) / steps;
+
+    var i = 0;
+    var timer = window.setInterval(function(){
+      if(++i >= steps) {
+        window.clearInterval(timer);
+        if(callback) {
+          callback();
+        }
+      }
+      map.setZoom(Math.round(startingZoom + stepChange * i));
+    }, 80);
+  };
+
+  var smoothPan = function(map, coords, callback) {
+    var mapCenter = map.getCenter();
+    coords = new google.maps.LatLng(coords);
+
+    var steps = 12;
+    var panStep = {lat: (coords.lat() - mapCenter.lat()) / steps, lng: (coords.lng() - mapCenter.lng()) / steps};
+
+    var i = 0;
+    var timer = window.setInterval(function(){
+      if(++i >= steps) {
+        window.clearInterval(timer);
+        if(callback) callback();
+      }
+      map.panTo({lat: mapCenter.lat() + panStep.lat * i, lng: mapCenter.lng() + panStep.lng * i});
+    }, 1000/30);
+  }; 
